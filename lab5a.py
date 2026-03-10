f54cd7c084952323dec0c44cc457c363a07e8b8a
Franz Rosete <frosete@myseneca.ca>
2026-03-10 05:37:10 -0400

    passed all test for lab5a

--- lab5a.py
+++ lab5a.py
@@ -3,14 +3,14 @@
 
 def read_file_string(file_name):
     # Takes file_name as string for a file name, returns its entire contents as a string
-    f = open('data.txt', 'r')
+    f = open(file_name, 'r')
     read_data = f.read()
     f.close()
     return read_data
 def read_file_list(file_name):
     # Takes a file_name as string for a file name, 
     # return its entire contents as a list of lines without new-line characters
-    f = open('data.txt', 'r')
+    f = open(file_name, 'r')
     read_data1 = f.readlines()
     
     for r in range(len(read_data1)):
